cf12c56ab4ebcc28e9e670aca8c0d5ad69707c36
Vlad Saraev <vlad.saraev@cultureamp.com>
2022-12-29 02:11:40 +1300

    refactor

--- day6/main.go
+++ day6/main.go
@@ -5,27 +5,25 @@ import (
 	"fmt"
 )
 
-func main() {
-	stream := helpers.GetInput("./day6/input.txt")
-
-	Part1(stream)
-	Part2(stream)
+type Stream struct {
+	Data string
 }
 
-func Part2(stream string) {
-	for i := 13; i < len(stream); i++ {
-		section := stream[i-13 : i+1]
-
-		if isUnique(section) {
-			fmt.Println(i + 1)
-			break
-		}
+func NewStreamer(data string) Stream {
+	return Stream{
+		Data: data,
 	}
 }
 
-func Part1(stream string) {
-	for i := 3; i < len(stream); i++ {
-		section := stream[i-3 : i+1]
+func main() {
+	stream := NewStreamer(helpers.GetInput("./day6/input.txt"))
+	stream.FindBy(3)
+	stream.FindBy(13)
+}
+
+func (s Stream) FindBy(n int) {
+	for i := n; i < len(s.Data); i++ {
+		section := s.Data[i-n : i+1]
 
 		if isUnique(section) {
 			fmt.Println(i + 1)
@@ -39,14 +37,11 @@ func isUnique(substr string) bool {
 
 	for _, c := range substr {
 		_, ok := store[c]
-
 		if ok {
 			return false
 		}
-
 		store[c] = true
 	}
 
 	return true
-
 }
