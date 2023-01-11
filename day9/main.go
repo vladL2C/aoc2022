979321ba7891183a20fabf84e984818a9bb37c05
Vlad Saraev <vlad.saraev@cultureamp.com>
2023-01-11 18:53:26 +1300

    part 2

--- day9/main.go
+++ day9/main.go
@@ -13,6 +13,11 @@ type coord struct {
 }
 
 func main() {
+	Part1()
+	Part2()
+}
+
+func Part1() {
 	raw := helpers.GetInput("./day9/input.txt")
 
 	data := strings.Split(raw, "\n")
@@ -40,10 +45,42 @@ func main() {
 
 	}
 
-	fmt.Println(len(visibility))
-
+	fmt.Println("PART 1:", len(visibility))
 }
 
+func Part2() {
+	raw := helpers.GetInput("./day9/input.txt")
+
+	data := strings.Split(raw, "\n")
+
+	head := coord{x: 0, y: 0}
+	tails := make([]coord, 9)
+
+	visibility := make(map[coord]struct{})
+
+	visibility[tails[0]] = struct{}{}
+
+	for _, line := range data {
+		instruction := strings.Split(line, " ")
+		direction, moves := GetDirection(instruction)
+
+		for i := 0; i < moves; i++ {
+			head = MoveHead(head, direction)
+			previousTail := head
+			for j := 0; j < len(tails); j++ {
+				tails[j] = MoveTailToHead(previousTail, tails[j])
+				previousTail = tails[j]
+				if j == len(tails)-1 {
+					visibility[tails[j]] = struct{}{}
+				}
+
+			}
+
+		}
+
+	}
+	fmt.Println("PART 2:", len(visibility))
+}
 func Touching(head, tail coord) bool {
 	diffX := math.Abs(float64(tail.x - head.x))
 	diffY := math.Abs(float64(tail.y - head.y))
@@ -52,19 +89,22 @@ func Touching(head, tail coord) bool {
 }
 
 func MoveTailToHead(head, tail coord) coord {
-	x, y := head.x-tail.x, head.y-tail.y
-	if x > 0 {
-		x = 1
-	} else if x < 0 {
-		x = -1
-	}
-	if y > 0 {
-		y = 1
-	} else if y < 0 {
-		y = -1
+	if !Touching(head, tail) {
+		x, y := head.x-tail.x, head.y-tail.y
+		if x > 0 {
+			x = 1
+		} else if x < 0 {
+			x = -1
+		}
+		if y > 0 {
+			y = 1
+		} else if y < 0 {
+			y = -1
+		}
+		return coord{tail.x + x, tail.y + y}
 	}
 
-	return coord{tail.x + x, tail.y + y}
+	return tail
 }
 
 func MoveHead(head coord, direction string) coord {
